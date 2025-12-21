737bcf9d73e41a11ac945682c086e8a082b725e4
Elia <127631993+eliaghazal@users.noreply.github.com>
2025-12-21 19:43:16 +0200

    Modify Hotel constructor to include ownerId and capacity

--- Vacationly/src/Model/Hotel.java
+++ Vacationly/src/Model/Hotel.java
@@ -1,8 +1,8 @@
 package Model;
 
 public class Hotel extends Place_Base {
-    public Hotel(String id, String name, String desc, String loc, double price) {
-        super(id, name, desc, loc, price);
+    public Hotel(String id, String ownerId, String name, String desc, String loc, double price, int capacity) {
+        super(id, ownerId, name, desc, loc, price, capacity);
     }
     @Override 
     public String getCategory() { return "Hotel"; }
