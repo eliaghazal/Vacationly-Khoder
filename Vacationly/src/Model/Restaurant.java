0d4895666df30cefe45a4ea959bc63f9dbaf6332
Elia <127631993+eliaghazal@users.noreply.github.com>
2025-12-21 19:43:33 +0200

    Update Restaurant constructor to include ownerId and capacity

--- Vacationly/src/Model/Restaurant.java
+++ Vacationly/src/Model/Restaurant.java
@@ -1,8 +1,8 @@
 package Model;
 
 public class Restaurant extends Place_Base {
-    public Restaurant(String id, String name, String desc, String loc, double price) {
-        super(id, name, desc, loc, price);
+    public Restaurant(String id, String ownerId, String name, String desc, String loc, double price, int capacity) {
+        super(id, ownerId, name, desc, loc, price, capacity);
     }
     @Override 
     public String getCategory() { return "Restaurant"; }
