aca3c16b602b8e1783451c0588c0e89ce5d6856a
Elia <127631993+eliaghazal@users.noreply.github.com>
2025-12-21 19:59:52 +0200

    Add recipientId to Message class

--- Vacationly/src/Model/Message.java
+++ Vacationly/src/Model/Message.java
@@ -5,20 +5,23 @@ import java.io.Serializable;
 public class Message implements Serializable {
     private String senderId;
     private String senderName;
+    private String recipientId; // Link to BusinessOwner ID
     private String content;
     private String response;
     private boolean isResolved;
 
-    public Message(String senderId, String senderName, String content) {
+    public Message(String senderId, String senderName, String recipientId, String content) {
         this.senderId = senderId;
         this.senderName = senderName;
+        this.recipientId = recipientId;
         this.content = content;
         this.isResolved = false;
         this.response = "";
     }
 
-    // Getters Setters
+    public String getSenderId() { return senderId; }
     public String getSenderName() { return senderName; }
+    public String getRecipientId() { return recipientId; }
     public String getContent() { return content; }
     public String getResponse() { return response; }
     public boolean isResolved() { return isResolved; }
