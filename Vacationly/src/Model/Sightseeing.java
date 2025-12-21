1487145ba4a0a65428d84a17974498dd051618a2
Elia <127631993+eliaghazal@users.noreply.github.com>
2025-12-21 19:44:14 +0200

    Modify Sightseeing constructor to include ownerId and capacity

--- Vacationly/src/Model/Sightseeing.java
+++ Vacationly/src/Model/Sightseeing.java
@@ -1,8 +1,8 @@
 package Model;
 
 public class Sightseeing extends Place_Base {
-    public Sightseeing(String id, String name, String desc, String loc, double price) {
-        super(id, name, desc, loc, price);
+    public Sightseeing(String id, String ownerId, String name, String desc, String loc, double price, int capacity) {
+        super(id, ownerId, name, desc, loc, price, capacity);
     }
     @Override 
     public String getCategory() { return "Sightseeing"; }
