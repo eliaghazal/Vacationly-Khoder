7286c634bab070e8530989862b0b9ad026fc8509
Elia <127631993+eliaghazal@users.noreply.github.com>
2025-12-21 19:30:26 +0200

    Enhance Client class with balance management
    Added balance and credit card fields with methods to manage balance.

--- Vacationly/src/Model/Client.java
+++ Vacationly/src/Model/Client.java
@@ -1,7 +1,22 @@
 package Model;
 
 public class Client extends User_Base {
-    public Client(String id, String username, String password, String fullName) {
-        super(id, username, password, fullName);
+    private double balance;
+    private String creditCard;
+
+    public Client(String id, String username, String password, String fullName, double initialBalance, String creditCard) {
+        super(id, username, password, fullName, "CLIENT");
+        this.balance = initialBalance;
+        this.creditCard = creditCard;
+    }
+
+    public double getBalance() { return balance; }
+    public void setBalance(double balance) { this.balance = balance; }
+    
+    public void deductBalance(double amount) throws Exception {
+        if (this.balance < amount) {
+            throw new Exception("Insufficient Funds");
+        }
+        this.balance -= amount;
     }
 }
